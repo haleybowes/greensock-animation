bd9487beb4a231feedec4b1b152e1dad761989ae
Haley Bowes <bramsonbowes.haley@gmail.com>
2018-06-14 13:31:38 -0400

    car animations

---
+++ script.js
@@ -0,0 +1,11 @@
+let tl = new TimelineLite()
+
+tl.to('.full-van', 0, { scale: 1.7, left: 120, bottom: 20, delay: 1 })
+
+tl.to('.frame-1', 1, {opacity: 0})
+
+tl.to('.full-van', 2, { scale: 1.7, bottom: 20, left: -200 })
+
+tl.to('.full-van', 2, { scale: 1, bottom: 0, left: -40 })
+
+
