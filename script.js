fc2034f90d091ce638809fb3e65d2771605ff31c
Haley Bowes <bramsonbowes.haley@gmail.com>
2018-06-14 16:12:01 -0400

    adding

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 let tl = new TimelineLite()
 
-tl.to('.full-van', 0, { scale: 1.7, left: 120, bottom: 20, delay: 1 })
 
-tl.to('.frame-1', 1, {opacity: 0})
+// tl.to('.full-van', 0, { scale: 1.7, left: 120, bottom: 20, delay: 1 })
 
-tl.to('.full-van', 2, { scale: 1.7, bottom: 20, left: -200 })
+// tl.from('.text-1', 1, {right: 200})
+// tl.to('.text-1', 1, { left: 300, delay:1 })
 
-tl.to('.full-van', 2, { scale: 1, bottom: 0, left: -40 })
+// tl.to('.frame-1', 1, {opacity: 0, delay: 1})
 
-tl.from('.get-more', 1, {opacity: 0})
+
+
+// tl.to('.full-van', 2, { scale: 1.7, bottom: 20, left: -200 })
+
+// tl.to('.full-van', 2, { scale: 1, bottom: 0, left: -40 })
+
+// tl.from('.get-more', 1, {opacity: 0})
 
 
 
