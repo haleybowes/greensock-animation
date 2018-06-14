3cc767045942b0c851208fed1fd62339f30e2ebe
Haley Bowes <bramsonbowes.haley@gmail.com>
2018-06-14 17:45:22 -0400

    button shine

--- script.js
+++ script.js
@@ -8,11 +8,18 @@ tl.to('.text-1', 1, { left: 300, delay:1 })
 
 tl.to('.frame-1', 1, {opacity: 0, delay: 1})
 
+tl.from('.text-2', 1, { right: 300 })
+tl.to('.text-2', 1, { left: 300, delay: 1 })
 
+tl.to('.full-van', 1, { scale: 1.7, bottom: 20, left: -200 })
 
-tl.to('.full-van', 2, { scale: 1.7, bottom: 20, left: -200 })
+tl.from('.text-3', 1, { right: 300 })
+tl.to('.text-3', 1, { left: 300, delay: 1 })
 
 tl.to('.full-van', 2, { scale: 1, bottom: 0, left: -40 })
+
+tl.from('.text-4', 1, { right: 300 })
+tl.from('.text-5', 1, { right: 300 })
 
 tl.from('.get-more', 1, {opacity: 0})
 
